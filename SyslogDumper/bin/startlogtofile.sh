f89e6d17ba041c135c6278f70f349be25a6f3a6d
Xiaming Chen <chenxm35@gmail.com>
2015-03-20 17:10:59 +0800

    Remove bug of SyslogDumper script

--- SyslogDumper/bin/startlogtofile.sh
+++ SyslogDumper/bin/startlogtofile.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 # Parameters
 OUTFOLDERr=/home/hadoop/wifi.syslog.original
-RORT=514
+PORT=514
 FILTER="all"
 FILESIZE=1000000000
 OPTS="-z -P $PORT -d $OUTFOLDER -s $FILESIZE -F $FILTER"
 
 # kill older running processes
-pid=$(ps -ef | grep $(basename $OUTFOLDER) | awk '$8 == "/usr/bin/java" {print$2}')
-kill $pid
+PID=$(ps -ef | grep $(basename $OUTFOLDER) | awk '$8 == "/usr/bin/java" {print$2}')
+kill $PID
 
 mkdir -p $OUTFOLDER
 
 # Get runnable package file
-BIN=`dirname "$0"`
-BIN=`cd "$bin">/dev/null; pwd`
-LHOME="${bin}/.."
+BIN=$(dirname "$0")
+BIN=$(cd "$BIN">/dev/null; pwd)
+LHOME="${BIN}/.."
 
-RUNJAR=`find $LHOME/target -name *-with-dependencies.jar`
+RUNJAR=$(find $LHOME/target -name *-with-dependencies.jar)
 
 if [[ -z "$RUNJAR" ]] || [[ "$RUNJAR" == " " ]]; then
     echo "Cannot find runable jar binary. Please run 'mvn package' first."
