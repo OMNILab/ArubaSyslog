41fe2b14ad2fe62394358f5a67cf540d12aca2e9
Xiaming Chen <chenxm35@gmail.com>
2015-03-20 16:29:27 +0800

    Optimize SyslogDumper's logger script

--- SyslogDumper/bin/WifiSyslogDayByDay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pid=$(ps -ef | grep WiFi.Syslog | awk '$8 == "/usr/bin/java" {print$2}')
-kill $pid
-
-/home/hadoop/workspace/ArubaSyslog/SyslogDumper/bin/startlogtofile.sh
--- SyslogDumper/bin/startlogtofile.sh
+++ SyslogDumper/bin/startlogtofile.sh
@@ -3,20 +3,25 @@
 set -e
 
 # Parameters
-dirname=/home/data/WiFi.Syslog
-port=514
-filter="all"
-filesize=1000000000
-OPTS="-z -P $port -d $dirname -s $filesize -F $filter"
+OUTFOLDERr=/home/hadoop/wifi.syslog.original
+RORT=514
+FILTER="all"
+FILESIZE=1000000000
+OPTS="-z -P $PORT -d $OUTFOLDER -s $FILESIZE -F $FILTER"
 
-mkdir -p $dirname
+# kill older running processes
+pid=$(ps -ef | grep $(basename $OUTFOLDER) | awk '$8 == "/usr/bin/java" {print$2}')
+kill $pid
 
-# Obtain runnable package file
-bin=`dirname "$0"`
-bin=`cd "$bin">/dev/null; pwd`
+mkdir -p $OUTFOLDER
+
+# Get runnable package file
+BIN=`dirname "$0"`
+BIN=`cd "$bin">/dev/null; pwd`
 LHOME="${bin}/.."
 
 RUNJAR=`find $LHOME/target -name *-with-dependencies.jar`
+
 if [[ -z "$RUNJAR" ]] || [[ "$RUNJAR" == " " ]]; then
     echo "Cannot find runable jar binary. Please run 'mvn package' first."
     exit -1;
